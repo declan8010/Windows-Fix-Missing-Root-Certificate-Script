cdc53a1a6e3d1c9447489dbc24d4de8bb15e478e
Declan1080 <declanemery6@gmail.com>
2024-01-11 20:22:57 +0000

    Added Admin Prompt and Output Reporting

--- Windows Root Certificate Script.bat
+++ Windows Root Certificate Script.bat
@@ -1,12 +1,20 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
+
+:: Check for administrator privileges
+NET SESSION >nul 2>&1
+if %errorLevel% neq 0 (
+    echo You must run this script as an administrator.
+    pause
+    exit /b 1
+)
 
 :menu
 cls
 echo Main Menu:
 echo 1. Check the number of certificates in Trusted Root Certification Authorities
-echo 2. Run the main script
-echo 3. Exit
+echo 2. Download/Update Certificates
+echo 3. Exit Script
 
 set /p choice=Enter your choice: 
 
@@ -18,8 +26,14 @@ if "%choice%"== "1" (
 )
 
 if "%choice%"== "2" (
-    :: Create folder C:\PS\
-    mkdir "C:\PS"
+    :: Check if folder C:\PS\ already exists
+    if not exist "C:\PS" (
+        mkdir "C:\PS"
+    )
+
+    :: Get the initial count of certificates in Trusted Root Certification Authorities
+    set /a initialCount=0
+    for /f %%A in ('powershell -command "(Get-ChildItem Cert:\LocalMachine\Root).Count"') do set /a initialCount=%%A
 
     :: Run certutil.exe to generate SST file
     certutil.exe -generateSSTFromWU C:\PS\roots.sst
@@ -27,11 +41,17 @@ if "%choice%"== "2" (
     :: Wait for 5 seconds
     timeout /t 5 /nobreak >nul
 
-    :: Run PowerShell script
+    :: Run PowerShell script to import certificates
     powershell -command "$sstStore = (Get-ChildItem -Path C:\ps\roots.sst); $sstStore | Import-Certificate -CertStoreLocation Cert:\LocalMachine\Root"
 
-    :: Show real-time output of the PowerShell script
-    powershell -command "$sstStore = (Get-ChildItem -Path C:\ps\roots.sst); $sstStore | Import-Certificate -CertStoreLocation Cert:\LocalMachine\Root"
+    :: Get the updated count of certificates in Trusted Root Certification Authorities
+    set /a updatedCount=0
+    for /f %%A in ('powershell -command "(Get-ChildItem Cert:\LocalMachine\Root).Count"') do set /a updatedCount=%%A
+
+    :: Display the number of added certificates and the new total
+    set /a addedCount=updatedCount - initialCount
+    echo Number of certificates added: !addedCount!
+    echo New total number of certificates: !updatedCount!
     pause
     goto menu
 )
